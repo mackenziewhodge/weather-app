25c63abf82a9169ba314f9d3c1bdca6f3392ed4f
mackenziewhodge <mackenziehodgeart@gmail.com>
2024-08-05 18:51:41 -0400

    integrated API, replaced placeholder information with real current data for glasgow\

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
+
+function updateWeatherData(response){
+let updateTemperature=document.querySelector("#update-temperature")
+ let cityElement=document.querySelector("#weather-city");
+  
+cityElement.innerHTML=response.data.city
+updateTemperature.innerHTML = Math.round(response.data.temperature.current)
+
+}
+
+
+function searchCity(city){
+let apiKey="9f6b8bcf34c1t0b4fa7o41dfc57380a8"
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
+`
+axios.get(apiUrl).then(updateWeatherData);
+}
+
+
 function handleSearchSubmit(event){
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input")
-  let cityElement=document.querySelector("#weather-city");
-
-  cityElement.innerHTML=searchInput.value
+  searchCity(searchInput.value);
 ;}
 
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("glasgow")
